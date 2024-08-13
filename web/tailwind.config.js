16b7a27982e5da5637d76b5dcf6864b4309ba494
Alan Kersaudy <alan.kersaudy@gmail.com>
2024-08-13 09:56:24 +0200

    add: shadcn-vue and its skeleton loader

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -79,6 +79,20 @@ export default {
           950: 'var(--accent-950)',
         },
       },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
   plugins: [],
