fd4c6b43e5fe602e43c7ff0edac9acda138b866a
Alan Kersaudy <alan.kersaudy@gmail.com>
2024-08-13 09:56:24 +0200

    add: shadcn-vue and its skeleton loader

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,22 +1,26 @@
 <template>
   <div class="h-[100em] flex flex-col w-full ">
     <div class="w-full px-6 sm:px-12 lg:px-16">
-      <div class="container">
-        <div
-          class="w-10 h-10 bg-secondary"
+      <div class="section-container flex flex-row justify-between">
+        <Skeleton
+          class="w-64 h-52 bg-secondary"
           v-for="card of cardsSkeletons"
           :key="card"
-        ></div>
+        ></Skeleton>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import { Skeleton } from '@/components/shadcn/ui/skeleton'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'HomeView',
+  components: {
+    Skeleton
+  },
   data: () => ({
     cardsSkeletons: 4,
   }),
