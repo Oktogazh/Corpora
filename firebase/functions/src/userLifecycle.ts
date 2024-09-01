dec50f4b83b8b788500e89dc10a172f5bd326809
Alan Kersaudy <alan.kersaudy@gmail.com>
2024-09-01 19:13:52 +0200

    fix(function): forgot to make function async

--- firebase/functions/src/userLifecycle.ts
+++ firebase/functions/src/userLifecycle.ts
@@ -3,12 +3,16 @@ import * as admin from "firebase-admin";
 const db = admin.firestore();
 
 
-const handleUserDeletion = functions.auth.user().onDelete((user) => {
-  const { displayName } = user;
-  if (typeof displayName === "string" && displayName.length > 0) {
-    db.collection("unique_usernames").doc(displayName).delete();
-  }
-});
+const handleUserDeletion = functions
+  .region('europe-west1')
+  .auth
+  .user()
+  .onDelete(async (user) => {
+    const { displayName } = user;
+    if (typeof displayName === "string" && displayName.length > 0) {
+      await db.collection("unique_usernames").doc(displayName).delete();
+    }
+  });
 
 export {
   handleUserDeletion,
