ab252d7597a31409286db0f50db6d2c9c48b88e7
Alan Kersaudy <alan.kersaudy@gmail.com>
2024-09-27 00:26:33 +0100

    trying to fix TS error

---
+++ web/src/types/firestoreDocTypes.ts
@@ -0,0 +1,35 @@
+import type { FieldValue } from "firebase-admin/firestore";
+import { type UserInfo } from "firebase-admin/auth";
+
+export type SegmentBasis = {
+  segment: string;
+  languageTag: string;
+}
+
+export type SegmentRefDoc = {
+  ref: string;
+  langtag: string;
+  createdAt: FieldValue,
+}
+
+export type LanguageDoc = {
+  lastActivity: FieldValue,
+  postsCount: FieldValue,
+}
+
+export type SegmentDoc = {
+  ownerUid: UserInfo["uid"];
+  langtag: string;
+  segment: string;
+  createdAt: FieldValue;
+}
+
+export type Post = SegmentDoc & {
+  id: string,
+  ownerUsername: string,
+}
+
+export declare interface UniqueUsername {
+  owner: UserInfo["uid"];
+  createdAt: FieldValue;
+}
